4f5f9f45ce6289b9ab85be9834d3c21773b83f9a
alexis stephan <alexis-stephan@hotmail.fr>
2023-11-16 23:00:59 +0100

    scaffold article
    Signed-off-by: alexis stephan <alexis-stephan@hotmail.fr>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  resources :articles, except: [:index] # les 7 routes du crud /articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
