5e943bd0be7c7572048e1f0c2852974ce2923f9e
alexis stephan <alexis-stephan@hotmail.fr>
2023-11-16 23:42:06 +0100

    with the seed
    Signed-off-by: alexis stephan <alexis-stephan@hotmail.fr>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
+require 'open-uri'
 puts 'destroy in progress'
 Article.destroy_all
 puts 'Destroy done'
 
 puts 'create articles'
-Article.create!(title: 'Super article', body: 'Wow comment faire pour déployer mon application')
+file = URI.open('https://images.unsplash.com/photo-1682685797406-97f364419b4a?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
+article = Article.new(title: 'Super article', body: 'Wow comment faire pour déployer mon application')
+article.pictures.attach(io: file, filename: 'test', content_type: 'jpg')
+article.save!
 puts 'done'
